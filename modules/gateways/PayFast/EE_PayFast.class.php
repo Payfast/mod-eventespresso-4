6ef5a8f9763495a76e8aa156c749ab88224984fc
Brendon Posen <brendon.posen@payfast.co.za>
2017-05-03 15:40:45 +0200

    update

--- modules/gateways/PayFast/EE_PayFast.class.php
+++ modules/gateways/PayFast/EE_PayFast.class.php
@@ -18,6 +18,10 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  *
  * Payment Gateway - PayFast
  *
+ * Copyright (c) 2008 PayFast (Pty) Ltd
+ * You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
+ *
  * @package         Event Espresso
  * @subpackage      gateways/
  * @author              Ron Darby - PayFast
